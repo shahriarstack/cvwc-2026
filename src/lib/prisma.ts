4a9053621fd13d7e17f3b9ed2cefe6f58f4d0db1
Motor-Sr.Executive <Shahriar.Imon@aci-bd.com>
2026-06-12 17:54:21 +0600

    Fix PrismaClient import endpoint for Neon adapter

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client/edge';
+import { PrismaClient } from '@prisma/client';
 import { Pool, neonConfig } from '@neondatabase/serverless';
 import { PrismaNeon } from '@prisma/adapter-neon';
 
